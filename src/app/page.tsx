f489a9f65d5b0ad60a4ee90a34bc22dddc219528
Josembro <josechiwila@gmail.com>
2025-07-09 16:06:41 +0000

    feat: estrutura inicial frontend Next.js + Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        RH Gestão — Frontend funcionando com TailwindCSS
+      </h1>
+    </main>
+  );
+}
